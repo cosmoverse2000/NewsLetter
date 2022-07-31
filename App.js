10d2172d4cdfc664d718e77dbe7f6115e2a81c4d
Anshul Patel <anshulpatel5757@gmail.com>
2022-07-31 16:03:06 +0530

    created an Subrciption app , with live api calss

---
+++ App.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+app.post("/", function (req, res) {
+
+    const data = {
+        email_address: req.body.email,
+        status: "subscribed",
+        merge_fields: {
+            FNAME: req.body.fName,
+            LNAME: req.body.lName,
+        }
+    }
+    const jsonDATA = JSON.stringify(data);
+    const url = "https://us18.api.mailchimp.com/3.0/lists/" + process.env.ID + "/members?skip_merge_validation=false";
+    const options = {
+        method: "POST",
+
+        auth: process.env.KEY
+
+    }
+
+    const requst = https.request(url, options, function (response) {
+
+        response.on("data", function (data) {
+            if (response.statusCode === 200) {
+                res.sendFile(__dirname + "/success.html")
+            }
+            else {
+                res.sendFile(__dirname + "/failure.html")
+
+                // res.send(JSON.parse(data).detail)
+            }
+
+        });
+    })
+
+    requst.write(jsonDATA);
+    requst.end();
+
+
+
+})
+
+app.post("/failure", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.listen(process.env.PORT || 3000, function () {//process.env.PORT-this is added to run this on heroku also
+    console.log("server started at 3000");
+})
+
---
+++ README.md
@@ -0,0 +1,45 @@
+# Newsletter  @ https://whispering-hamlet-93814.herokuapp.com/
+An newsletter signup app made using node - express, request, body-parser, API requests and callbacks.
+
+TO FIND THE SUBSCRIBED AUDIENCE LIST GO HERE, THIS IS THE API PROVIDER FOR NEWLETTER SUBS.
+ALSO REGARDS API AND PARAMS IS GIVEN IN WEBSITE ONLY.
+https://us18.admin.mailchimp.com/lists/members?id=291340#p:1-s:25-sa:last_update_time-so:false
+SIGNIN WITH CREDs.
+
+
+
+
+BASIC START:
+(see notes on node lectures VERY MUCH HELPFUL)
+
+-create folder
+-npm init
+-create main file
+
+to be edited....
+
+
+
+
+TO RUN :
+
+- node app.js
+
+(here app.js is the file name in which listen port id there) 
+
+or else if installed nodemon(an npm package)
+
+-nodemon app.js
+
+
+
+
+TO INSTALL DEPENDENCIES:
+- npm i
+(if already package.json is ready)
+
+
+-npm init
+-npm i package_name
+
+
